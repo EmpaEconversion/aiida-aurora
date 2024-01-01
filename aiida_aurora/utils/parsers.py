f4301255ef076c6a903cbca555b93d71a1f4f1ad
Edan Bainglass <edan.bainglass@gmail.com>
2024-01-01 10:14:06 +0000

    Discard unused `time-cycles` key

--- aiida_aurora/utils/parsers.py
+++ aiida_aurora/utils/parsers.py
@@ -120,7 +120,6 @@ def post_process_data(t: np.ndarray, Ewe: np.ndarray, I: np.ndarray) -> dict:
         "Ewe": Ewe,
         "I": I,
         "cycle-number": np.arange(len(Qd)),
-        "time-cycles": t[idx[2::2]],
         "Q": cumtrapz(I, t, axis=0, initial=0) / 3.6,
         "Qd": np.array(Qd) / 3.6,
         "Qc": np.array(Qc) / 3.6,
