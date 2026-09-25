1899ce571bb9114d4cab4dbcb9fbac30ded6d58b
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2026-09-25 09:23:10 +0200

    Bump pandas, aiida-core

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,15 @@
 """pytest fixtures for simplified testing."""
 import pytest
 
-pytest_plugins = ["aiida.manage.tests.pytest_fixtures"]
+pytest_plugins = ["aiida.tools.pytest_fixtures"]
 
 
 @pytest.fixture(scope="function", autouse=True)
-def clear_database_auto(clear_database):  # pylint: disable=unused-argument
+def clear_database_auto(aiida_profile_clean):  # pylint: disable=unused-argument
     """Automatically clear database in between tests."""
 
 
 @pytest.fixture(scope="function")
-def aurora_code(aiida_local_code_factory):
+def aurora_code(aiida_code_installed):
     """Get a aurora code."""
-    return aiida_local_code_factory(executable="diff", entry_point="aurora")
+    return aiida_code_installed(default_calc_job_plugin="aurora", filepath_executable="diff")
